e7bb688592e0132e6b8335ef13e3fac9ce23f110
hariomfw21 <darkphinixgamer@gmail.com>
2023-06-17 04:08:34 +0530

    javascript code for displaying and filtring doctor in DoctorPage , javascript code for Single Doctor Display, added Appointment Date and Appointment time in Appointment Schema

--- Backendd/connection/connection.js
+++ Backendd/connection/connection.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize")
 
-const sequelize = new Sequelize("heal_care","root","Saif@5038",{
+const sequelize = new Sequelize("heal_care","root","iqoo",{
     host:'localhost',
     dialect:"mysql"
 })
